fc44121159fe7f06c3d2bb45eed8cbe0b02593d7
fractalswift <matthewthomasmacklin@gmail.com>
2020-05-31 17:42:01 +0100

    added manifest and icon for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,25 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Just 4 Funk Productions",
+        short_name: "Just 4 Funk",
+        start_url: "/",
+        background_color: "orange",
+        theme_color: "orange",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    "gatsby-plugin-offline",
+    {
       resolve: "gatsby-plugin-html-attributes",
       options: {
         lang: "en",
