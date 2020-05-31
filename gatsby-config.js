20ef1ab11c28ca96f219d0da754d96c8cff110dd
fractalswift <matthewthomasmacklin@gmail.com>
2020-05-31 17:52:04 +0100

    changed name from default in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -172,7 +172,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Just 4 Funk Productions`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
